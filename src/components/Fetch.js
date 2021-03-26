b4269a493dddce94d5fcef43f93f8d111fc76e8f
brewchetta <chett.tiller@gmail.com>
2021-03-26 16:47:16 -0400

    added description for useFetchState example

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -10,6 +10,9 @@ export default function Fetch() {
 
     <div className="example">
 
+      <p>This is a cool one! It's a stateful hook like useState however it's "setter" callback accepts a URL / options and will fetch data, parse the JSON, and automatically set the state!</p>
+      <p>It also includes a "busy signal" so you can introduce a loading spinner or something similar while the fetch is firing.</p>
+
       {busySignal ? <p>Loading...</p> : null}
 
       <button onClick={() => fetchCatFacts(URL)}>Fetch Cat Facts</button>
